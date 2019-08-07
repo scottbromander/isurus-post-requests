e251b19c680ca56b7f2110fcf6e7a4d6a482b015
Scott Bromander <scottbromander@gmail.com>
2019-08-07 15:36:41 -0500

    initial commit

---
+++ server/public/scripts/client.js
@@ -0,0 +1,13 @@
+$(document).ready(init);
+
+function init() {
+    $.ajax({
+        type: 'GET',
+        url: '/trains',
+    }).then(function(response) {
+       for(let train of response) {
+        $('.js-container').append(`<p>${train.name}</p>`)
+       }
+    });
+
+}
--- server/server.js
+++ server/server.js
@@ -1,33 +1,45 @@
 // bring express into your project
 // you will need to `npm init` and `npm install express` first
-
+const express = require('express');
 
 // create your express app
+const app = express();
 
 // This is your array of trains
 const trains = [
     { name: 'Thomas', color: 'Blue' },
     { name: 'Gordon', color: 'Blue' },
     { name: 'Henry', color: 'Green' },
-    { name: 'James', color: 'Red' }
+    { name: 'James', color: 'Red' },
+    { name: 'Jessie', color: 'Yellow'},
 ];
 
 // -------- BASE -----//
 
+app.use(express.static('server/public'));
+
 // Create your `/train` route here
 // when a user visits localhost:5000/train
 // this route should return the array of trains
+app.get('/trains', (req,res) => {
+    res.send(trains); //array
+});
 
 
 // Create your `/first-train` route here
 // when a user visits localhost:5000/first-train
 // this route should return the first train in the array
+app.get('/first-train', (req,res) => {
+    res.send(trains[0]); //object
+})
 
 
 // Create your `/last-train` route here
 // when a user visits localhost:5000/last-train
 // this route should return the last train in the array
-
+app.get('/last-train', (req,res) => {
+    res.send(trains[trains.length - 1]); //object
+});
 
 
 // -------- STRETCH -----//
@@ -37,13 +49,27 @@ const trains = [
 // this route should return the number of trains in the array
 // NOTE: express doesn't like it when you return numbers
 // instead, return an object like { totalCount: 4 }
-
+app.get('/count', (req,res) => {
+    res.send({totalCount: trains.length});
+})
 
 // Create your `/random` route here
 // when a user visits localhost:5000/random
 // this route should return a single train at random
-
+app.get('/random', (req,res) => {
+    const ranNumber = randomNumber(0, trains.length-1);
+    const randomTrain = trains[ranNumber];
+    res.send(randomTrain);
+})
 
 // -------- BASE -----//
 
 // Don't forget to start your app by running `.listen()`
+app.listen(5000, () => {
+    console.log('Totes running on port 5000!');
+})
+
+
+function randomNumber(min, max){
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
