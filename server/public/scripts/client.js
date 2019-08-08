48493b58ce069d7ba06048cf48216f5164c347f4
Scott Bromander <scottbromander@gmail.com>
2019-08-08 10:14:26 -0500

    update for post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,12 +1,25 @@
 $(document).ready(init);
 
 function init() {
+    // $.ajax({
+    //     type: 'GET',
+    //     url: '/trains',
+    // }).then(function(response) {
+    //    for(let train of response) {
+    //     $('.js-container').append(`<p>${train.name}</p>`)
+    //    }
+    // });
+
+    const newTrain = {
+        name: 'Jessie',
+        color: 'Yellow',
+    }
+
     $.ajax({
-        type: 'GET',
+        type: 'POST',
         url: '/trains',
+        data: newTrain,
     }).then(function(response) {
-       for(let train of response) {
-        $('.js-container').append(`<p>${train.name}</p>`)
-       }
+       alert(response);
     });
 }
