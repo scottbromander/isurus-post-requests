a41af2e6c719857967c7eb24eb056f2d4f615d3e
Scott Bromander <scottbromander@gmail.com>
2019-08-08 11:01:00 -0500

    adds post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,25 +1,57 @@
 $(document).ready(init);
 
 function init() {
-    // $.ajax({
-    //     type: 'GET',
-    //     url: '/trains',
-    // }).then(function(response) {
-    //    for(let train of response) {
-    //     $('.js-container').append(`<p>${train.name}</p>`)
-    //    }
-    // });
+    $('#js-form-add-train').on('submit', submitTrain);
 
-    const newTrain = {
-        name: 'Jessie',
-        color: 'Yellow',
-    }
+    getTrains();
+}
+
+function submitTrain(event) {
+    event.preventDefault();
+
+    const formArray = $(this).serializeArray();
+
+    const dataForServer = {
+        name: $('#js-form-name').val(),
+        color: $('#js-form-color').val(),
+    };
+
+    // for (let dataFromForm of formArray) {
+    //     dataForServer[dataFromForm.name] = dataFromForm.value;
+    //  Create a new key on our object and set it equal to the value
+    //  both new key name and value are provided by the inputs in html.
+    //  This method works for as many inputs you have, without adding code!
+    // }
+
+
+    console.log(dataForServer);
 
     $.ajax({
         type: 'POST',
         url: '/trains',
-        data: newTrain,
+        data: dataForServer,
     }).then(function(response) {
-       alert(response);
+        getTrains();
     });
+}
+
+function getTrains() {
+    $.ajax({
+        type: 'GET',
+        url: '/trains',
+    }).then(function(response) {
+       render(response);
+    });
+}
+
+function render(response) {
+    const listOfTrains = response;
+    $('.js-container').empty();
+    for (let train of listOfTrains) {
+        $('.js-container').append(`
+         <div>
+             <p>${train.name} - ${train.color}</p>
+         </div>
+        `)
+    }
 }
